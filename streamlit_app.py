bf215c44cec0c58f7a7b227666657079611c6d6e
sanketbankar80-star <sanketbankar80@gmail.com>
2026-06-13 00:22:05 +0530

    Fix variable name typo in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
-pd_df=mydataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
